0e84802233fb90bcdb4f73e556369add42cc1add
devds <rostyslav0128@gmail.com>
2017-02-27 18:01:28 +0200

    Comprehension S1+2

--- www/templates/story1/videoStoryOneRecordVoice/videoStoryOneRecordVoiceController.js
+++ www/templates/story1/videoStoryOneRecordVoice/videoStoryOneRecordVoiceController.js
@@ -1,46 +1,149 @@
 angular.module('LegendSingapore.videoStoryOneRecordVoice.ctrl', [])
   .controller('videoStoryOneRecordVoiceController',  ['$scope', 'ngAudio', function($scope, ngAudio) {
 
-    // START ------ Playlist of voices for characters
+    // START ______ Playlist of voices for characters
     $scope.duration = 100;
     $scope.tracks =
     {
-      audio1: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-1.mp3'),
-      audio2: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-2.mp3'),
-      audio3: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-3.mp3'),
-      audio4: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-4.mp3'),
-      audio5: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-5.mp3'),
-      audio6: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-6.mp3'),
-      audio7: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-7.mp3'),
-      audio8: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-8.mp3'),
-      audio9: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-9.mp3'),
-      audio10: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-10.mp3'),
-      audio11: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-11.mp3'),
-      audio12: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-12.mp3'),
-      audio13: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU-13.mp3'),
+      audio1: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_1.mp3'),
+      audio2: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_2.mp3'),
+      audio3: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_3.mp3'),
+      audio4: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_4.mp3'),
+      audio5: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_5.mp3'),
+      audio6: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_6.mp3'),
+      audio7: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_7.mp3'),
+      audio8: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_8.mp3'),
+      audio9: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_9.mp3'),
+      audio10: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_10.mp3'),
+      audio11: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_11.mp3'),
+      audio12: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_12.mp3'),
+      audio13: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNU_13.mp3'),
       audioIntro1: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNUtama_Intro_1.mp3'),
       audioIntro2: ngAudio.load('templates/story1/videoStoryOneRecordVoice/audio_s1/SNUMamanda_Intro2.mp3')
     };
-    // END ------ Playlist of voices for characters
+    // END ______ Playlist of voices for characters
 
-    //Start ------ Poems_story1___
+    //Start ______ Poems_story1___
     $scope.track.volume = 1;
     // $scope.track.play();
     $scope.duration = 100;
     $scope.poems =[{
-      poem1_au:  ngAudio.load('templates/story1/videoStoryOneRecordVoice/pantun/Pantun-SNU-1.mp3'),
+      poem1_au:  ngAudio.load('templates/story1/videoStoryOneRecordVoice/pantun/Pantun_SNU_1.mp3'),
       showContent1: false,
       poem_cont: 'Pulaunya indah berpasir putih,\n Namanya Temasek haluan dituju; \n Sang Nila Utama semakin gigih, \n Hendak membuka negara baharu.'
     },{
-      poem1_au: ngAudio.load('templates/story1/videoStoryOneRecordVoice/pantun/Pantun-SNU-2.mp3'),
+      poem1_au: ngAudio.load('templates/story1/videoStoryOneRecordVoice/pantun/Pantun_SNU_2.mp3'),
       showContent1: false,
       poem_cont: 'Hendak bermalam di Kota Kluang, \n   Berjalan jauh bukan kepalang; \n    Semua barangan telah dibuang, \n        Namun badai menjadi penghalang.'
     },{
-      poem1_au: ngAudio.load('templates/story1/videoStoryOneRecordVoice/pantun/Pantun-SNU-3.mp3'),
+      poem1_au: ngAudio.load('templates/story1/videoStoryOneRecordVoice/pantun/Pantun_SNU_3.mp3'),
       showContent1: false,
       poem_cont: 'Pergi berburu di tengah hutan, \n  Membawa bersama barang keperluan; \n  Mahkota dibuang di tengah lautan, \n  Bahtera selamat  ke tempat tujuan.'
     }];
-//END ------ Poems_story1___
+//END ______ Poems_story1___
+
+
+
+    <!--START         Comprehension       questions -->
+    $scope.pantun1 = [{
+      done: false,
+
+      audio: ngAudio.load('templates/story1/videoStoryOneRecordVoice/comprehension/Comprehension-SNU-Q1.mp3'),
+      question: "Apakah yang telah terjadi kepada bahtera yang mereka naiki pada awal cerita?",
+      answers: [
+        {
+          done: false,
+          title: "A",
+          answer: "Bahtera hampir karam. ",
+          success: true
+        },
+        {
+          done: false,
+          title: "B",
+          answer: "Bahtera dipukul ombak.",
+          success: false
+        },
+        {
+          done: false,
+          title: "C",
+          answer: "Bahtera telah tenggelam.",
+          success: false
+        }]
+    },
+      {
+        done: false,
+        audio: ngAudio.load('templates/story1/videoStoryOneRecordVoice/comprehension/Comprehension-SNU-Q2.mp3'),
+        question: "Apakah yang Sang Nila Utama terpaksa buang pada akhirnya?",
+        answers: [
+          {
+            done: false,
+            title: "A",
+            answer: "Hartanya.",
+            success: false
+          },
+          {
+            done: false,
+            title: "B",
+            answer: "Mahkotanya",
+            success: true
+          },
+          {
+            done: false,
+            title: "C",
+            answer: "Barang-barang bahteranya.",
+            success: false
+          }]
+      },
+      {
+        done: false,
+        audio: ngAudio.load('templates/story1/videoStoryOneRecordVoice/comprehension/Comprehension-SNU-Q3.mp3'),
+        question: "Bagaimanakah kesudahan nasib bahtera Sang Nila Utama  itu?",
+        answers: [
+          {
+            done: false,
+            title: "A",
+            answer: "Karam di lautan.",
+            success: false
+          },
+          {
+            done: false,
+            title: "B",
+            answer: "Pecah dipukul ombak.",
+            success: false
+          },
+          {
+            done: false,
+            title: "C",
+            answer: "Selamat mendarat di pulau.",
+            success: true
+          }]
+      },
+      {
+        done: false,
+        audio: ngAudio.load('templates/story1/videoStoryOneRecordVoice/comprehension/Comprehension-SNU-Q4.mp3'),
+        question: "Binatang apa yang dilihat Sang Nila Utama?",
+        answers: [
+          {
+            done: false,
+            title: "A",
+            answer: "Harimau.",
+            success: false
+          },
+          {
+            done: false,
+            title: "B",
+            answer: "Singa.",
+            success: true
+          },
+          {
+            done: false,
+            title: "C",
+            answer: "Serigala.",
+            success: false
+          }]
+      }];
+    <!--END         Comprehension       questions -->
+
 
   $scope.formParams = {};
   $scope.stage = "";
--- www/templates/story2/videoStoryTwoRecordVoice/videoStoryTwoRecordVoiceController.js
+++ www/templates/story2/videoStoryTwoRecordVoice/videoStoryTwoRecordVoiceController.js
@@ -1,104 +1,227 @@
 angular.module('LegendSingapore.videoStoryTwoRecordVoice.ctrl', [])
-  .controller('videoStoryTwoRecordVoiceController',  ['$scope', 'ngAudio', function($scope,ngAudio) {
+    .controller('videoStoryTwoRecordVoiceController', ['$scope', 'ngAudio', function ($scope, ngAudio) {
 
-    // START ------ Playlist of voices for characters
-    $scope.duration = 100;
-    $scope.tracks =
-    {
-      audio1_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-1.mp3'),
-      audio2_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-2.mp3'),
-      audio3_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-3.mp3'),
-      audio4_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-4.mp3'),
-      audio5_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-5.mp3'),
-      audio6_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-6.mp3'),
-      audio7_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-7.mp3'),
-      audio8_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-8.mp3'),
-      audio9_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-9.mp3'),
-      audio10_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-10.mp3'),
-      audio11_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-11.mp3'),
-      audio12_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-12.mp3'),
-      audio13_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-13.mp3'),
-      audio14_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB-14.mp3'),
-      audioIntro1_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_Intro_1.mp3'),
-      audioIntro2_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_Intro_2.mp3')
-    };
+        // START ------ Playlist of voices for characters
+        $scope.duration = 100;
+        $scope.countQuestion = 0;
+        $scope.tracks =
+        {
+            audio1_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_1.mp3'),
+            audio2_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_2.mp3'),
+            audio3_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_3.mp3'),
+            audio4_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_4.mp3'),
+            audio5_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_5.mp3'),
+            audio6_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_6.mp3'),
+            audio7_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_7.mp3'),
+            audio8_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_8.mp3'),
+            audio9_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_9.mp3'),
+            audio10_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_10.mp3'),
+            audio11_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_11.mp3'),
+            audio12_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_12.mp3'),
+            audio13_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_13.mp3'),
+            audio14_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_14.mp3'),
+            audioIntro1_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_Intro_1.mp3'),
+            audioIntro2_s2: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/audio_s2/PB_Intro_2.mp3')
+        };
 
-    $scope.track.volume = 1;
-    // $scope.track.play();
-    // END ------ Playlist of voices for characters
-    $scope.duration = 100;
-    $scope.poems =[{
-      poem1_au:  ngAudio.load('templates/story2/videoStoryTwoRecordVoice/pantun/Pantun-PB-1.mp3'),
-      showContent: false,
-      poem_cont: 'Pergi memanah di Tanjung Ara,\n Busar dibalut si kain baldu; \n Mawar dan Melur dua bersaudara, \n Ke mana sahaja bersama selalu.'
-    },{
-      poem1_au: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/pantun/Pantun-PB-2.mp3'),
-      showContent: false,
-      poem_cont: 'Sakit terasa tidak terperi, \n   Jari terketuk tukul dan pahat; \n    Melur ditangkap dibawa lari, \n    Mawar berazam adik  diselamat.'
-    },{
-      poem1_au: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/pantun/Pantun-PB-3.mp3'),
-      showContent: false,
-      poem_cont: 'Berenang-renang ikan tenggiri, \n  Mengikut arus arah utara \n  Kasih saudara kekal abadi, \n  Menjadi pulau dua bersaudara.'
-    }];
+        $scope.track.volume = 1;
+        // $scope.track.play();
+        // END ______ Playlist of voices for characters
+        $scope.duration = 100;
+        $scope.poems = [{
+            poem1_au: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/pantun/Pantun_PB_1.mp3'),
+            showContent: false,
+            poem_cont: 'Pergi memanah di Tanjung Ara,\n Busar dibalut si kain baldu; \n Mawar dan Melur dua bersaudara, \n Ke mana sahaja bersama selalu.'
+        }, {
+            poem1_au: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/pantun/Pantun_PB_2.mp3'),
+            showContent: false,
+            poem_cont: 'Sakit terasa tidak terperi, \n   Jari terketuk tukul dan pahat; \n    Melur ditangkap dibawa lari, \n    Mawar berazam adik  diselamat.'
+        }, {
+            poem1_au: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/pantun/Pantun_PB_3.mp3'),
+            showContent: false,
+            poem_cont: 'Berenang_renang ikan tenggiri, \n  Mengikut arus arah utara \n  Kasih saudara kekal abadi, \n  Menjadi pulau dua bersaudara.'
+        }];
+
+        $scope.pantun = [{
+            done: false,
+
+            audio: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/comprehension/Comprehension-PB-Q1.mp3'),
+            question: "Siapakah Mawar dan Melur? ",
+            answers: [
+                {
+                    done: false,
+                    title: "A",
+                    answer: "Mereka merupakan sepasang kawan.",
+                    success: false
+                },
+                {
+                    done: false,
+                    title: "B",
+                    answer: "Mereka merupakan sepasang sepupu.",
+                    success: false
+                },
+                {
+                    done: false,
+                    title: "C",
+                    answer: "Mereka merupakan sepasang adik beradik.",
+                    success: true
+                }]
+        },
+            {
+                done: false,
+                audio: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/comprehension/Comprehension-PB-Q2.mp3'),
+                question: "Mengapakah Mawar dan Melur berlari?",
+                answers: [
+                    {
+                        done: false,
+                        title: "A",
+                        answer: "Mereka telah dikejar oleh sekumpulan lanun.",
+                        success: true
+                    },
+                    {
+                        done: false,
+                        title: "B",
+                        answer: "Mereka telah dikepung oleh sekumpulan lanun",
+                        success: false
+                    },
+                    {
+                        done: false,
+                        title: "C",
+                        answer: "Mereka telah ditangkap oleh sekumpulan lanun.",
+                        success: false
+                    }]
+            },
+            {
+                done: false,
+                audio: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/comprehension/Comprehension-PB-Q3.mp3'),
+                question: "Apakah yang dilakukan Mawar setelah Melur dibawa lari?",
+                answers: [
+                    {
+                        done: false,
+                        title: "A",
+                        answer: "Mawar mengekori lanun-lanun tersebut.",
+                        success: true
+                    },
+                    {
+                        done: false,
+                        title: "B",
+                        answer: "Mawar menentang lanun-lanun tersebut.",
+                        success: false
+                    },
+                    {
+                        done: false,
+                        title: "C",
+                        answer: "Mawar memperdaya lanun-lanun tersebut.",
+                        success: false
+                    }]
+            },
+            {
+                done: false,
+                audio: ngAudio.load('templates/story2/videoStoryTwoRecordVoice/comprehension/Comprehension-PB-Q4.mp3'),
+                question: "Mengapakah Melur terjun ke laut?",
+                answers: [
+                    {
+                        done: false,
+                        title: "A",
+                        answer: "Melur mahu menyelamatkan diri.",
+                        success: true
+                    },
+                    {
+                        done: false,
+                        title: "B",
+                        answer: "Melur mahu berjumpa dengan kakaknya Minah.",
+                        success: false
+                    },
+                    {
+                        done: false,
+                        title: "C",
+                        answer: "Melur mahu mengalahkan lanun-lanun tersebut.",
+                        success: false
+                    }]
+            }];
 
 
 
-  $scope.formParams = {};
-  $scope.stage = "";
-  $scope.formValidation = false;
-  $scope.toggleObjView = false;
+        $scope.formParams = {};
+        $scope.stage = "";
+        $scope.formValidation = false;
+        $scope.toggleObjView = false;
 
-  $scope.formParams = {
-    cCEmail: '',
-    cCEmailList: []
-  };
+        $scope.formParams = {
+            cCEmail: '',
+            cCEmailList: []
+        };
 
-  // Navigation functions
-  $scope.next = function (stage) {
-    //$scope.direction = 1;
-    //$scope.stage = stage;
+        // Navigation functions
+        $scope.next = function (stage) {
+            //$scope.direction = 1;
+            //$scope.stage = stage;
 
-    $scope.formValidation = true;
+            $scope.formValidation = true;
 
-    if ($scope.multiStepForm.$valid) {
-      $scope.direction = 1;
-      $scope.stage = stage;
-      $scope.formValidation = false;
-    }
-  };
+            if ($scope.multiStepForm.$valid) {
+                $scope.direction = 1;
+                $scope.stage = stage;
+                $scope.formValidation = false;
+            }
+        };
 
-  $scope.back = function (stage) {
-    $scope.direction = 0;
-    $scope.stage = stage;
-  };
+        $scope.back = function (stage) {
+            $scope.direction = 0;
+            $scope.stage = stage;
+        };
 
-  // CC email list functions
-  $scope.addCCEmail = function () {
-    $scope.rowId++;
+        // CC email list functions
+        $scope.addCCEmail = function () {
+            $scope.rowId++;
 
-    var email = {
-      email: $scope.formParams.cCEmail,
-      row_id: $scope.rowId
-    };
+            var email = {
+                email: $scope.formParams.cCEmail,
+                row_id: $scope.rowId
+            };
 
-    $scope.formParams.cCEmailList.push(email);
+            $scope.formParams.cCEmailList.push(email);
 
-    $scope.formParams.cCEmail = '';
-  };
+            $scope.formParams.cCEmail = '';
+        };
 
-  $scope.removeCCEmail = function (row_id) {
-    for (var i = 0; i < $scope.formParams.cCEmailList.length; i++) {
-      if ($scope.formParams.cCEmailList[i].row_id === row_id) {
-        $scope.formParams.cCEmailList.splice(i, 1);
-        break;
-      }
-    }
-  };
+        $scope.removeCCEmail = function (row_id) {
+            for (var i = 0; i < $scope.formParams.cCEmailList.length; i++) {
+                if ($scope.formParams.cCEmailList[i].row_id === row_id) {
+                    $scope.formParams.cCEmailList.splice(i, 1);
+                    break;
+                }
+            }
+        };
 
-  $scope.reset = function() {
-    // Clean up scope before destorying
-    $scope.formParams = {};
-    $scope.stage = "";
-  };
+        $scope.reset = function () {
+            // Clean up scope before destorying
+            $scope.formParams = {};
+            $scope.stage = "";
+        };
 
-}]);
+        // $scope.myform = true;
+        // $scope.formData = true;
+        // if (myform.formData.$valid) {
+        //     addClass("black")
+        //     $scope.stage = stage;
+        //     $scope.formValidation = false;
+        // }
+        // $scope.next = function (stage) {
+        //     //$scope.direction = 1;
+        //     //$scope.stage = stage;
+        //
+        //     $scope.formValidation = true;
+        //
+        //     if ($scope.multiStepForm.$valid) {
+        //         $scope.direction = 1;
+        //         $scope.stage = stage;
+        //         $scope.formValidation = false;
+        //     }
+        // };
+        //   console.log('hhh');
+        //   var result = document.getElementsByClassName("multi-files");
+        //   $('.comprehension_box_s2 input[value="1"]')
+        // }
+
+    }]);
